1cc2f6639c58283d0fa5598a362eb23139862c4f
nickidahl <6417280@rts-365.dk>
2020-11-16 10:25:43 +0100

    andet commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import Login from './views/Login';
+
 
 function App() {
   return (
     <div className="App">
-      
+      <Login />
+
+
     </div>
   );
 }
---
+++ src/views/Login.js
@@ -0,0 +1,10 @@
+/** @jsxImportSource @emotion/core */;
+
+
+const Login = () => {
+    return ( 
+        <h1>Hey</h1>
+     );
+}
+ 
+export default Login;
